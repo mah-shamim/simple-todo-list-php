03d7f42400b41adb9f4a9ef7721afaf2cf6f7599
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2024-07-30 02:06:48 +0600

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$dbname = 'todolist';
+$user = 'root'; // Change if necessary
+$pass = ''; // Change if necessary
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
+?>
