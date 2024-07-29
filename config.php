7fef4969177a4c3b9068962abf04b4f12be5e1a9
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2024-07-30 02:30:41 +0600

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,12 @@
 <?php
+
+// Database configuration
 $host = 'localhost';
 $dbname = 'todolist';
 $user = 'root'; // Change if necessary
 $pass = ''; // Change if necessary
 
+// Create a new PDO instance
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
